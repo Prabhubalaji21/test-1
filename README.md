69dc74e315bb04493734464862b20107426c6d3a
Prabhubalaji21 <107781010+Prabhubalaji21@users.noreply.github.com>
2022-07-26 06:27:29 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# guvi task
+ it was task submission repository
---
+++ script.js
@@ -0,0 +1,57 @@
+// var a = 5;
+// var b = "value";
+// var c = true;
+// var x = a;
+// var y = b;
+// var z = c;
+// x = 4;
+// y = "guvi";
+// z = null;
+
+// console.log(a,b,c,x,y,z);
+//copy by value
+
+// var arr1 = [1,2,3];
+// var obj1 = { name: "prabhu", age :25};
+// var arr2 = arr1;
+// var obj2 = obj1;
+
+// arr2[1] = 6;
+// obj1.name = "balaji";
+// console.log(arr1,arr2,obj1,obj2);
+// copy by reference
+//json java script object notation
+var resume = {
+    "Name" : "PRABHU BALAJI B",
+    "Educational qualification" : "B.E COMPUTER SCIENCE ENGINEER",
+    "email" : "prabhubalaji73@gmail.com",
+    "Phone no" : "9047547524",
+    "Gender" : "male",
+    "Father's name" : "balasubramanian.s",
+    "Date of birth" : "21-06-1997",
+    "Nationality" : "Indian",
+    "Languages known" : "english , tamil",
+    "Address" : "no.70 , 4th street, k.c.samy nagar, bagayam, otteri , vellore 632002...",
+    "Career objective" : "Looking for a challenging job in reputed organization that provides learning and promotional environment, based on skill level and personal contribution to the company’s success",
+    "Software skills" : "MS office , adobe premium pro",
+    "Languages known" : "c,c++,java,javascript",
+    "Area of interest" : "Developing and testing , Network Support , Video Editing",
+    "Certified courses" : "Completed Diploma In Computer Applications at Pace Computer Education Institute with 90%",
+    "Technical skills" : "Programming In C, C++, Java basics , Be the technical support  in the Android Application developed by our college team , Completed Internship & Inplant training on Java & Android Application development , Conducted Seminar about Ethical Hacking has a Cyber Warrior in our college",
+
+};
+console.log(resume);
+//-----------output for json format----------
+//-----json format-----
+
+//-------object format-------
+// var resume = {
+//     name : "PRABHU BALAJI B",
+//     educationalqualification : "B.E COMPUTER SCIENCE ENGINEER",
+//     email : "prabhubalaji73@gmail.com"
+// };
+
+
+
+//console.log(resume.name);
+//------output for object format------
